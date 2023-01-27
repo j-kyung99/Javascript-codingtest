94c6f7f2900df450425b12ad6ddec5c9d7996535
j-kyung99 <86589951+j-kyung99@users.noreply.github.com>
2023-01-27 22:25:21 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -0,0 +1,12 @@
+function solution(numer1, denom1, numer2, denom2) {
+    let denom = denom1 * denom2;
+    let numer = (numer1 * denom2) + (numer2 * denom1);
+    let answer = 1;
+    for(let i = 2; i <= denom; i++){
+        if(denom % i === 0 && numer % i === 0){
+            answer = i;
+        }
+    }
+    
+    return [numer/answer, denom/answer];
+}
