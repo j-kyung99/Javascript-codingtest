a3a58b761c8632c35bd4fac1e4a60a2fe35574d2
j-kyung99 <86589951+j-kyung99@users.noreply.github.com>
2023-01-28 17:27:31 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.js
+++ 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.js
@@ -1,8 +1,3 @@
 function solution(array) {
-    for(let i=0;i<array.length-1;i++){
-        array.sort((a,b) => a-b);
-    }
-    const middle = Math.floor(array.length/2);
-    console.log(middle);
-    return array[middle];
+    return array.sort((a, b) => a - b)[Math.floor(array.length/2)];
 }
