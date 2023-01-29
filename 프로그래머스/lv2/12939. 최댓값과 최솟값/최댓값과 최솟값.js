c31ebd79a71e3b05bc40892891ab03716b0d4f3a
j-kyung99 <86589951+j-kyung99@users.noreply.github.com>
2023-01-29 22:10:24 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.05 ms, Memory: 33.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,3 +1,4 @@
 function solution(s) {
-    return s.split(' ').sort((a,b) => a-b)[0] + ' ' + s.split(' ').sort((a,b) => a-b)[s.split(' ').length - 1];
+    const a = s.split(' ');
+    return Math.min(...a) + ' ' + Math.max(...a);
 }
