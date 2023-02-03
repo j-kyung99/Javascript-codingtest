41ab12610596067c1e27e750d894ebd03f79979b
j-kyung99 <86589951+j-kyung99@users.noreply.github.com>
2023-02-03 20:00:34 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
@@ -1,3 +1,3 @@
 function solution(my_string) {
-    return my_string.split('').filter(a => ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'].includes(a) ? true : false).map(a => Number(a)).sort((a,b) => a-b);
+    return my_string.split('').filter(a => !isNaN(a)).map(a => Number(a)).sort((a,b) => a-b);
 }
