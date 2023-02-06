07b974d43e2b91376dc3d1dd797726de12e5a2fd
j-kyung99 <86589951+j-kyung99@users.noreply.github.com>
2023-02-06 21:24:33 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
+++ 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
@@ -1,10 +1,4 @@
 function solution(array) {
-    const a = [...array].sort((a,b) => b-a);
-    const answer = [];
-    for(let i=0;i<array.length;i++){
-        if(a[0] === array[i]) {
-            answer.push(a[0], i);
-        }
-    }
-    return answer;
+    let max = Math.max(...array);
+    return [max, array.indexOf(max)];
 }
