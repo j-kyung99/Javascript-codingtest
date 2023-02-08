017cfebec864e6d064d2ac10e5f6d201e82a046c
j-kyung99 <86589951+j-kyung99@users.noreply.github.com>
2023-02-08 14:19:01 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
+++ 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
@@ -1,8 +1,3 @@
 function solution(array, height) {
-    let count = 0;
-    array.push(height);
-    array.sort((a,b) => a-b).map((v) => {
-        if(v > height) count++;
-    });
-    return count;
+    return array.filter((v) => v > height).length;
 }
