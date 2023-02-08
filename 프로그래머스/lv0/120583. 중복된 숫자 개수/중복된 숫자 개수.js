8b08fb85d0ec9a32e101257bd6d4e45882551555
j-kyung99 <86589951+j-kyung99@users.noreply.github.com>
2023-02-08 14:12:40 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120583. 중복된 숫자 개수/중복된 숫자 개수.js
+++ 프로그래머스/lv0/120583. 중복된 숫자 개수/중복된 숫자 개수.js
@@ -1,7 +1,3 @@
 function solution(array, n) {
-    let count = 0;
-    for(let i=0;i<array.length;i++){
-        if(array[i] === n) count++;
-    }
-    return count;
+    return array.filter((v) => v===n).length;
 }
