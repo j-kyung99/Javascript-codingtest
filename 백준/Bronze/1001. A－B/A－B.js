49248225ad710ed6957264c39230cfe4c0223ade
j-kyung99 <86589951+j-kyung99@users.noreply.github.com>
2023-05-10 14:29:29 +0900

    [Bronze V] Title: A-B, Time: 128 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().split(" ");
+let a = +input[0];
+let b = +input[1];
+
+solution(a, b);
+
+function solution(a, b) {
+  console.log(a - b);
+}
