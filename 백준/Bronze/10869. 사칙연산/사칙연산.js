0d6fd73901a9e552fcf55c8fad5bcca83bd2d684
j-kyung99 <86589951+j-kyung99@users.noreply.github.com>
2023-05-10 14:37:54 +0900

    [Bronze V] Title: 사칙연산, Time: 128 ms, Memory: 9604 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().split(" ");
+let a = +input[0];
+let b = +input[1];
+
+solution(a, b);
+
+function solution(a, b) {
+  console.log(a + b);
+  console.log(a - b);
+  console.log(a * b);
+  console.log(parseInt(a / b));
+  console.log(a % b);
+}
