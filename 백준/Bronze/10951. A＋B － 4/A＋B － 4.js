350b1b3e9e94b76b0b6e2b20ae3e0fe779fe02c1
j-kyung99 <86589951+j-kyung99@users.noreply.github.com>
2023-05-12 11:55:43 +0900

    [Bronze V] Title: A+B - 4, Time: 136 ms, Memory: 9576 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+for (let i = 0; i < input.length; i++) {
+  let test = input[i].split(" ").map(Number);
+  console.log(test[0] + test[1]);
+}
