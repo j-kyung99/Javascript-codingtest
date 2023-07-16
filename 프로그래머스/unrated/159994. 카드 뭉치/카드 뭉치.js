e34c173397aee68d6169ad5551465f5b79f952fe
j-kyung99 <86589951+j-kyung99@users.noreply.github.com>
2023-07-16 19:27:20 +0900

    [unrated] Title: 카드 뭉치, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.js
@@ -0,0 +1,13 @@
+function solution(cards1, cards2, goal) {
+    let count = 0;
+    for(let i=0;i<goal.length;i++){
+        if(cards1[0] === goal[i]){
+            count++;
+            cards1.shift();
+        } else if(cards2[0] === goal[i]){
+            count++;
+            cards2.shift();
+        }
+    }
+    return count === goal.length ? "Yes" : "No";
+}
