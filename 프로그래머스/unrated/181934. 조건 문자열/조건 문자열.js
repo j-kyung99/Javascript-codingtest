2decf283363237f78efc4deacfbc682ea6156faa
j-kyung99 <86589951+j-kyung99@users.noreply.github.com>
2023-08-04 10:29:53 +0900

    [unrated] Title: 조건 문자열, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.js
@@ -0,0 +1,14 @@
+function solution(ineq, eq, n, m) {
+    if(eq === '='){
+        if(ineq === '>'){
+            return n >= m ? 1 : 0;
+        }
+        else return n <= m ? 1 : 0;
+    }
+    else {
+        if(ineq === ">"){
+            return n > m ? 1 : 0;
+        }
+        else return n < m ? 1 : 0;
+    }
+}
