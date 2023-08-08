4471dae0380aa0327100b9d1829fdcdd1690038f
j-kyung99 <86589951+j-kyung99@users.noreply.github.com>
2023-08-08 15:24:40 +0900

    [unrated] Title: 수 조작하기 1, Time: 2.62 ms, Memory: 37 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.js
@@ -0,0 +1,15 @@
+function solution(n, control) {
+    control = control.split('');
+    for(let i=0;i<control.length;i++){
+        if(control[i] === 'w'){
+            n++;
+        } else if(control[i] === 's'){
+            n--;
+        } else if(control[i] === 'd'){
+            n += 10;
+        } else {
+            n -= 10;
+        }
+    }
+    return n;
+}
