5b567831ce8f34309aee17f402f401432c56b3ef
j-kyung99 <86589951+j-kyung99@users.noreply.github.com>
2023-08-13 09:34:03 +0900

    [unrated] Title: 접두사인지 확인하기, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.js
@@ -0,0 +1,7 @@
+function solution(my_string, is_prefix) {
+    let str = [];
+    for(let i=0;i<my_string.length;i++){
+        str.push(my_string.slice(0, i));
+    }
+    return str.indexOf(is_prefix) >= 0 ? 1 : 0;
+}
