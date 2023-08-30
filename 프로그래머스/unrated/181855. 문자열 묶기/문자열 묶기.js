d3d730e04026050f72f2c3bf9bfcbc31b0861fab
j-kyung99 <86589951+j-kyung99@users.noreply.github.com>
2023-08-30 10:25:33 +0900

    [unrated] Title: 문자열 묶기, Time: 67.16 ms, Memory: 56.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181855. 문자열 묶기/문자열 묶기.js
@@ -0,0 +1,13 @@
+function solution(strArr) {
+    const mappingTable = {};
+    
+    const max_len = Math.max(...strArr.map(item => item.length));
+    
+    for(let i = 1; i <= max_len; i++) {
+        const data = strArr.filter(item => item.length === i);
+        
+        mappingTable[i] = data.length;
+    }
+
+    return Math.max(...Object.values(mappingTable))
+}
